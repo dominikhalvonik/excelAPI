7abeb496e8e7266f2a4eda3887e6795b3cb0535e
dominikhalvonik <dominik.halvonik@principio.sk>
2017-05-19 12:49:11 +0200

    Change configuration and add test

---
+++ module/ExportAPI/test/Factory/Model/ExportAPITest/Factory/Model/ManageFilesFactoryTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dominik
+ * Date: 19.05.2017
+ * Time: 12:48
+ */
+
+namespace ExportAPITest\Factory\Model;
+
+use ExportAPI\Factory\Model\ManageFilesFactory;
+
+class ManageFilesFactoryTest extends \PHPUnit_Framework_TestCase
+{
+
+}
